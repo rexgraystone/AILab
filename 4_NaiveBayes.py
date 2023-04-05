8fc1b73fa5b2d049e4cfe46c5953f502eab0f133
Raghav Nanjappan <r6nanjappan@gmail.com>
2023-04-05 10:19:28 +0530

    Update programs

--- 4_NaiveBayes.py
+++ 4_NaiveBayes.py
@@ -1,23 +1,22 @@
 import re
 from collections import defaultdict
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
 class NaiveBayes:
-    def __init__(self, classes):
+    def __init__(self, classes: list):
         self.classes = classes
         self.vocab = set()
         self.class_word_counts = defaultdict(lambda: defaultdict(int))
         self.class_doc_counts = defaultdict(int)
         self.num_docs = 0
         
-    def preprocess(self, text):
-        # Remove punctuations and convert to lowercase
+    def preprocess(self, text: str) -> list:
         text = re.sub(r'[^\w\s]', '', text).lower()
-        # Remove stop words
         stop_words = set(['a', 'an', 'the', 'in', 'on', 'at', 'of', 'to', 'for', 'by', 'with', 'from', 'and'])
         tokens = text.split()
         tokens = [token for token in tokens if token not in stop_words]
         return tokens
-        
+
     def train(self, documents):
         for document, category in documents:
             tokens = self.preprocess(document)
@@ -43,23 +42,46 @@ class NaiveBayes:
     
 docs = [('The sky is blue', 'weather'), 
         ('The sun is bright', 'weather'),
+        ('It is raining', 'weather'),
         ('The news is depressing', 'politics'),
+        ('The election is coming up', 'politics'),
+        ('The election has ended', 'politics'),
+        ('The economy is doing good', 'economy'),
+        ('He lost his job because of the economy', 'economy'),
         ('The economy is improving', 'economy'),
         ('The movie was great', 'entertainment'),
-        ('I love pizza', 'food'),
+        ('I saw the movie at home', 'entertainment'),
+        ('I liked the movie', 'entertainment'),
+        ('I like to eat pizza', 'food'),
+        ('I just ate lunch', 'food'),
+        ('I eat rice once a day', 'food'),
         ('The game was exciting', 'sports'),
         ('The team played poorly', 'sports'),
-        ('The election is coming up', 'politics')]
+        ('The players were tired after the match', 'sports')]
 
 nb = NaiveBayes(['weather', 'politics', 'economy', 'entertainment', 'food', 'sports'])
 nb.train(docs)
 
-new_doc1 = 'ELECTION is coming up'
-new_doc2 = 'The sun is bright'
-new_doc3 = 'The economy is improving'
-category1 = nb.predict(new_doc1)
-category2 = nb.predict(new_doc2)
-category3 = nb.predict(new_doc3)
-print(f'The document "{new_doc1}" belongs to the category "{category1}"')
-print(f'The document "{new_doc2}" belongs to the category "{category2}"')
-print(f'The document "{new_doc3}" belongs to the category "{category3}"')
+phrase_1 = 'The movie was nice'
+phrase_2 = 'The sun is bright'
+phrase_3 = 'The economy is improving'
+phrase_4 = 'I like to eat noodles'
+phrase_5 = 'ELECTION is coming up'
+phrase_6 = 'Did you watch the GAME last night'
+category1 = nb.predict(phrase_1)
+category2 = nb.predict(phrase_2)
+category3 = nb.predict(phrase_3)
+category4 = nb.predict(phrase_4)
+category5 = nb.predict(phrase_5)
+category6 = nb.predict(phrase_6)
+print(f'The phrase "{phrase_1}" belongs to the category "{category1}"')
+print(f'The phrase "{phrase_2}" belongs to the category "{category2}"')
+print(f'The phrase "{phrase_3}" belongs to the category "{category3}"')
+print(f'The phrase "{phrase_4}" belongs to the category "{category4}"')
+print(f'The phrase "{phrase_5}" belongs to the category "{category5}"')
+print(f'The phrase "{phrase_6}" belongs to the category "{category6}"')
+true_vals = ['entertainment', 'weather', 'economy', 'food', 'politics', 'sports']
+pred_vals = [category1, category2, category3, category4, category5, category6]
+print(confusion_matrix(true_vals, pred_vals))
+print(accuracy_score(true_vals, pred_vals))
+print(classification_report(true_vals, pred_vals))
