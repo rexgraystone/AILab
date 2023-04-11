eafed9aeaa140288e4c07ad295294bf6046c8912
Raghav Nanjappan <r6nanjappan@gmail.com>
2023-04-12 05:20:41 +0530

    Update programs

--- 4_NaiveBayes.py
+++ 4_NaiveBayes.py
@@ -5,23 +5,17 @@ from sklearn.naive_bayes import MultinomialNB
 
 X_train = ['I love playing sports', 'Politics is my passion', 'Sports are fun', 'Politics is boring']
 y_train = ['sports', 'politics', 'sports', 'politics']
-
 vectorizer = CountVectorizer(stop_words='english')
-
 X_train_counts = vectorizer.fit_transform(X_train)
 
-clf = MultinomialNB()
-clf.fit(X_train_counts, y_train)
+model = MultinomialNB()
+model.fit(X_train_counts, y_train)
 
 X_test = ['I enjoy watching sports on TV', 'Politics is important for our country', 'i love politics']
-
 X_test_counts = vectorizer.transform(X_test)
-
-y_pred = clf.predict(X_test_counts)
+y_pred = model.predict(X_test_counts)
 
 output = []
-for i in range(len(X_test)):
+for index, i in enumerate(range(len(X_test))):
     output.append([X_test[i], y_pred[i]])
-
-for o in output:
-    print(o)
+    print(output[index])
