e9f5771d6d79bf0814cb1cb808a9e07d5dd46af5
Raghav Nanjappan <r6nanjappan@gmail.com>
2023-04-19 10:09:53 +0530

    Update programs

--- 7_NamedEntity.py
+++ 7_NamedEntity.py
@@ -6,14 +6,15 @@ import nltk
 # nltk.download('maxent_ne_chunker')
 # nltk.download('words') # Uncomment these lines if the specified files aren't downloaded
 
-sentence = """Python is an interpreted, high-level and general-purpose programming language"
-       "Pythons design philosophy emphasizes code readability with"
-       "its notable use of significant indentation."
-       "Its language constructs and object-oriented approach aim to"
-       "help programmers write clear and"
-       "logical code for small and large-scale projects"""
+paragraph = """Scarface is a 1983 American crime drama film directed by Brian De Palma and written by Oliver Stone. \
+    Loosely based on the 1929 novel of the same name and serving as a loose remake of the 1932 film,\
+    it tells the story of Cuban refugee Tony Montana (Al Pacino), \
+    who arrives penniless in Miami during the Mariel boatlift and becomes a powerful and extremely homicidal drug lord.\
+    The film co-stars Steven Bauer, Michelle Pfeiffer, Mary Elizabeth Mastrantonio and Robert Loggia.\
+    De Palma dedicated this version of Scarface to the memories of Howard Hawks and Ben Hecht, \
+    the writers of the original film."""
 
-for sent in nltk.sent_tokenize(sentence):
-    for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+for sentence in nltk.sent_tokenize(paragraph):
+    for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sentence))):
         if hasattr(chunk, 'label'):
             print(f"{' '.join(c[0] for c in chunk):<35} {chunk.label()}")
