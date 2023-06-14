8503bb54bf22cd2f5303ac1345060cf7f264e4d5
Raghav Nanjappan <r6nanjappan@gmail.com>
2023-06-14 09:55:42 +0530

    Update programs

--- 8_HandwrittenRecognition.py
+++ 8_HandwrittenRecognition.py
@@ -8,9 +8,9 @@ import numpy as np
 import random
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-tem = random.randint(1, 1000)
-images = X_train[tem]
-labels = y_train[tem]
+temp = random.randint(1, 1000)
+img = X_train[temp]
+val = y_train[temp]
 X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
 X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
 X_train = X_train.astype('float32') / 255.
@@ -31,10 +31,10 @@ score = model.evaluate(X_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 predictions = model.predict(X_test)
-x = model.predict(images.reshape(1, 28, 28, 1))
-plt.imshow(images)
+pred = model.predict(img.reshape(1, 28, 28, 1))
+plt.imshow(img)
 plt.show()
-np.argmax(x, axis=1)
-number = np.where(x==1.)[1]
+np.argmax(pred, axis=1)
+number = np.where(pred==1.)[1]
 number = str(number).lstrip('[').rstrip(']')
 print(f'The model has predicted that the image is of the number {number}.')
